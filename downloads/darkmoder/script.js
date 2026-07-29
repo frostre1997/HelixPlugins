8b948491d26ef22b6b9abbdd0881da2364f33b4c
Frostre_ <n9043395@gmail.com>
2026-07-29 20:13:53 +0200

    Delete downloads directory

--- downloads/darkmoder/script.js
+++
@@ -1,1 +0,0 @@
-console.log("Darkmoder has been loaded");
--- downloads/nodefaultfonts/script.js
+++
@@ -1,5 +0,0 @@
-console.log("nodefaultfonts has been loaded");
-
-// You can also dynamically change fonts via JS
-// For example, change to system font:
-// document.body.style.fontFamily = "system-ui, sans-serif";
