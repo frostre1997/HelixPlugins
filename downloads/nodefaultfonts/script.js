e8f78802ae95bc3e0e2c3ccd1ca03f420a84c81f
Frostre_ <n9043395@gmail.com>
2026-07-29 16:46:14 +0200

    Create script.js

---
+++ downloads/nodefaultfonts/script.js
@@ -0,0 +1,5 @@
+console.log("nodefaultfonts has been loaded");
+
+// You can also dynamically change fonts via JS
+// For example, change to system font:
+// document.body.style.fontFamily = "system-ui, sans-serif";
